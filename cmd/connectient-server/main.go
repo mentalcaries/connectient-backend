72916d7fef3ba6aea841484b89fe78eefe23a158
mentalcaries <neexis@gmail.com>
2025-11-15 13:27:50 -0400

    get db url from env, set up apiConfig

--- cmd/connectient-server/main.go
+++ cmd/connectient-server/main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/mentalcaries/connectient-backend/internal/database"
 	"github.com/mentalcaries/connectient-backend/internal/routes"
 )
 
 type apiConfig struct {
 	platform  string
 	jwtSecret string
+	db        *database.Queries
 }
 
 func main() {
-
 	const port = 4000
+
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	dbQueries := database.New(dbConn)
+	apiCfg := apiConfig{
+		db: dbQueries,
+	}
 
 	router := routes.NewRouter()
 	addr := fmt.Sprintf(":%v", port)
